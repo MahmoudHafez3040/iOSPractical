d70eddc3573e7dfed68f855f2ff00bded05a3284
MahmoudHafezz <mahmoud.hafez@ihorizons.com>
2020-04-12 18:33:17 +0200

    Initial Commit

---
+++ iOSPractical/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSPractical
+//
+//  Created by MahmoudHafez on 4/12/20.
+//  Copyright © 2020 MahmoudHafez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
